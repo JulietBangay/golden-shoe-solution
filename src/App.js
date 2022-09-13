0487be2f7537a0303bd3b45a0e8678dca07ce2da
JulietBangay <julietabangay@gmail.com>
2022-09-13 16:51:22 +0100

    Added navbar layout and styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,52 @@
+import { Routes, Route } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+import Navbar from "./components/navbar/Navbar";
+import Home from "./home/Home";
 import "./App.css";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#b30000",
+      //dark red
+    },
+    secondary: {
+      main: "#8798AD",
+      // grey
+      // #EBF1F4 (light grey)
+    },
+    error: {
+      main: "#ff0000",
+      //bright red
+    },
+    warning: {
+      main: "#E38A17",
+      // orange
+    },
+    info: {
+      main: "#ff00bf",
+      //bright pink
+    },
+    success: {
+      main: "#b30086",
+      // magenta
+    },
+    contrastThreshold: 3,
+    tonalOffset: 0.2,
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,49 @@
+import { Grid } from "@mui/material";
+import "./Navbar.css";
+import Logo from "../../images/example-logo-golden-shoe.png";
+
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      <div>
+        <Grid container alignItems="center" className="mainNav">
+          <Grid item xs={3} className="navItem" style={{ height: "100%" }}>
+            <img src={Logo} alt="Golden Shoe" className="navbarLogo" />
+          </Grid>
+          <Grid
+            item
+            xs={1}
+            className="navItem"
+            style={{ height: "100%" }}
+          ></Grid>
+          <Grid item xs={2} style={{ height: "100%" }}>
+            <button className="navButton">Home</button>
+          </Grid>
+          <Grid item xs={2} style={{ height: "100%" }}>
+            <button className="navButton">Products</button>
+          </Grid>
+          <Grid item xs={2} style={{ height: "100%" }}>
+            <button className="navButton">My Account</button>
+          </Grid>
+          <Grid item xs={2} style={{ height: "100%" }}>
+            <button className="navButton">My Bag</button>
+          </Grid>
+        </Grid>
+      </div>
+      <div className="miniNav">
+        <Grid container>
+          <Grid item xs={9}></Grid>
+          <Grid item xs={1} className="navText">
+            Help {"&"} FAQs
+          </Grid>
+          <Grid item xs={1} className="navText">
+            Contact
+          </Grid>
+          <Grid item xs={1} className="navText">
+            About us
+          </Grid>
+        </Grid>
+      </div>
+    </div>
+  );
+}
