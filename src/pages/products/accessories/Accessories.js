dd88141ff412aac58f510b53490ad61aca35d3a3
JulietBangay <julietabangay@gmail.com>
2022-09-15 15:56:14 +0100

    Basic products pages

--- src/pages/products/accessories/Accessories.js
+++ src/pages/products/accessories/Accessories.js
@@ -1,5 +1,69 @@
 import "./Accessories.css";
 
+import { Grid } from "@mui/material";
+import ShoeImage from "../../../images/shoe.png";
+
 export default function Accessories() {
-  return <div className="accessories">Hello</div>;
+  return (
+    <div className="accessories">
+      <div className="productsTitle">Bags and Accessories</div>
+      <div className="productsContainer">
+        <Grid container>
+          <Grid item xs={3}></Grid>
+          <Grid item xs={3}></Grid>
+          <Grid item xs={3}>
+            <div className="sortFilter">Sort</div>
+          </Grid>
+          <Grid item xs={3}>
+            <div className="sortFilter">Filter</div>
+          </Grid>
+        </Grid>
+        <Grid container spacing={2} className="productsGrid">
+          <Grid item xs={4}>
+            <div className="productThumbContainer">
+              <img src={ShoeImage} alt="" className="productImage" />
+              <div className="productName">Product Name</div>
+              <Grid container>
+                <Grid item xs={6}>
+                  <div className="productInformation">Product Information</div>
+                </Grid>
+                <Grid item xs={6}>
+                  <div className="productPrice">Product Price</div>
+                </Grid>
+              </Grid>
+            </div>
+          </Grid>
+          <Grid item xs={4}>
+            <div className="productThumbContainer">
+              <img src={ShoeImage} alt="" className="productImage" />
+              <div className="productName">Product Name</div>
+              <Grid container>
+                <Grid item xs={6}>
+                  <div className="productInformation">Product Information</div>
+                </Grid>
+                <Grid item xs={6}>
+                  <div className="productPrice">Product Price</div>
+                </Grid>
+              </Grid>
+            </div>
+          </Grid>
+          <Grid item xs={4}>
+            <div className="productThumbContainer">
+              <img src={ShoeImage} alt="" className="productImage" />
+              <div className="productName">Product Name</div>
+              <Grid container>
+                <Grid item xs={6}>
+                  <div className="productInformation">Product Information</div>
+                </Grid>
+                <Grid item xs={6}>
+                  <div className="productPrice">Product Price</div>
+                </Grid>
+              </Grid>
+            </div>
+          </Grid>
+        </Grid>
+        <button className="loadMoreButton">Load More</button>
+      </div>
+    </div>
+  );
 }
--- src/pages/products/women/Women.js
+++ src/pages/products/women/Women.js
@@ -1,5 +1,68 @@
 import "./Women.css";
+import { Grid } from "@mui/material";
+import ShoeImage from "../../../images/shoe.png";
 
 export default function Women() {
-  return <div className="women">hello</div>;
+  return (
+    <div className="women">
+      <div className="productsTitle">Womens Shoes</div>
+      <div className="productsContainer">
+        <Grid container>
+          <Grid item xs={3}></Grid>
+          <Grid item xs={3}></Grid>
+          <Grid item xs={3}>
+            <div className="sortFilter">Sort</div>
+          </Grid>
+          <Grid item xs={3}>
+            <div className="sortFilter">Filter</div>
+          </Grid>
+        </Grid>
+        <Grid container spacing={2} className="productsGrid">
+          <Grid item xs={4}>
+            <div className="productThumbContainer">
+              <img src={ShoeImage} alt="" className="productImage" />
+              <div className="productName">Product Name</div>
+              <Grid container>
+                <Grid item xs={6}>
+                  <div className="productInformation">Product Information</div>
+                </Grid>
+                <Grid item xs={6}>
+                  <div className="productPrice">Product Price</div>
+                </Grid>
+              </Grid>
+            </div>
+          </Grid>
+          <Grid item xs={4}>
+            <div className="productThumbContainer">
+              <img src={ShoeImage} alt="" className="productImage" />
+              <div className="productName">Product Name</div>
+              <Grid container>
+                <Grid item xs={6}>
+                  <div className="productInformation">Product Information</div>
+                </Grid>
+                <Grid item xs={6}>
+                  <div className="productPrice">Product Price</div>
+                </Grid>
+              </Grid>
+            </div>
+          </Grid>
+          <Grid item xs={4}>
+            <div className="productThumbContainer">
+              <img src={ShoeImage} alt="" className="productImage" />
+              <div className="productName">Product Name</div>
+              <Grid container>
+                <Grid item xs={6}>
+                  <div className="productInformation">Product Information</div>
+                </Grid>
+                <Grid item xs={6}>
+                  <div className="productPrice">Product Price</div>
+                </Grid>
+              </Grid>
+            </div>
+          </Grid>
+        </Grid>
+        <button className="loadMoreButton">Load More</button>
+      </div>
+    </div>
+  );
 }
